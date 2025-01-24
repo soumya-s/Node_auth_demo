4fb286c89f21213b4e26a2a60af96e057b00d598
soumya <twinklesou23@gmail.com>
2025-01-24 22:39:19 +0530

    login authentication

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const createError = require('http-errors');
 const User         =  require('../Models/user.model')
 const {authSchema} = require('../helpers/validationSchema')
+const {signAccessToken} = require('../helpers/jwthelper')
 
 
 router.post('/register',async(req,res,next)=>{
@@ -19,17 +20,27 @@ router.post('/register',async(req,res,next)=>{
         
         const user = new User(result)
         const savedUser = await user.save();   
-        res.send(savedUser);
+        const accessToken = await signAccessToken(savedUser.id)
+        res.send(accessToken);
     }
     catch(error){
-        if(error.isJo === true) error.status = 422
+        if(error.isJoi === true) error.status = 422
         next(error);
 
     }
 
 })
-router.post('/login',async(req,res) => {
-    res.send("Login route");
+router.post('/login',async(req,res,next) => {
+try{
+    const result = await authSchema.validateAsync(req.body);
+    res.send(result);
+}catch(error){
+    
+    if(error.isJoi === true){
+        return next(createError.BadRequest('Invalid username / password'));
+        next(error);
+    }
+  }
 })
 
 router.post('/refresh-token',async(req,res) => {
